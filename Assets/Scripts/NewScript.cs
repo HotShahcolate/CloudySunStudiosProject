374ce463959b3b32701e2dedf69d863ab6ed0134
Jade Wilds <jwilds6@gatech.edu>
2025-06-03 23:07:58 -0400

    jwilds6, commit 1

---
+++ Assets/Scripts/NewScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+
+    //Start is called before the first frame update
+    private void Start()
+    {
+        print("Hello");
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
