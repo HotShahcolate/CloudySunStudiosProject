1b70574139ff2d9aef598f4f5eef523675ebcf82
Mirza, Shah Jahan <smirza8@gatech.edu>
2025-07-20 17:27:40 -0400

    Update RespawnPlayer.cs

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -22,6 +22,7 @@ public class RespawnPlayer : MonoBehaviour
         player.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         player.GetComponent<Rigidbody>().MovePosition(respawnPoint.position);
         player.GetComponent<Rigidbody>().useGravity = true;
+        transform.position = respawnPoint.position;
 
         print("Player Respawned at: " + player.transform.position + player.name);
     }
