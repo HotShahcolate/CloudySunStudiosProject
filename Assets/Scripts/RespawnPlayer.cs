a6a3386abb81e0e9d2059547536ca79dfc6b0bc0
Snigdha_P <snig08@tamu.edu>
2025-07-22 00:45:11 -0500

    Platform of death beneath the game, corrected respawn logic

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -3,29 +3,59 @@ using UnityEngine;
 public class RespawnPlayer : MonoBehaviour
 {
     public Transform respawnPoint;
-
     public Transform checkpoint1;
-    public Transform checkpoint2; 
+    public Transform checkpoint2;
+    public Transform checkpoint3;
     public Transform distanceCheckPoint;
 
     private bool checkpoint1Reached = false;
     private bool checkpoint2Reached = false;
+    private bool checkpoint3Reached = false;
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float distance = Vector3.Distance(player.transform.position, distanceCheckPoint.position);
+
+        if (distance < 197f)
+        {
+            respawnPoint = checkpoint2;
+        }
+        else if (distance < 502f)
+        {
+            respawnPoint = checkpoint1;
+        }
+        else
+        {
+            respawnPoint = checkpoint3;
+        }
+
+        checkpoint1Reached = false;
+        checkpoint2Reached = false;
+        checkpoint3Reached = false;
+    }
 
     void Update()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         float distance = Vector3.Distance(player.transform.position, distanceCheckPoint.position);
 
-        if (!checkpoint1Reached && distance >= 197f)
+        if (!checkpoint2Reached && distance >= 197f)
+        {
+            respawnPoint = checkpoint2;
+            checkpoint2Reached = true;
+        }
+
+        if (!checkpoint1Reached && distance >= 502f)
         {
             respawnPoint = checkpoint1;
             checkpoint1Reached = true;
         }
 
-        if (!checkpoint2Reached && distance >= 1805f)
+        if (!checkpoint3Reached && distance >= 1805f)
         {
-            respawnPoint = checkpoint2;
-            checkpoint2Reached = true;
+            respawnPoint = checkpoint3;
+            checkpoint3Reached = true;
         }
     }
 
@@ -43,7 +73,6 @@ public class RespawnPlayer : MonoBehaviour
         {
             player.GetComponent<Collider>().enabled = true;
         }
-
         player.GetComponent<Rigidbody>().useGravity = false;
         player.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         player.GetComponent<Rigidbody>().MovePosition(respawnPoint.position);
